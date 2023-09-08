0d8eed5145d40c937332d0f16dde09d3b66d9a36
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-09 02:40:34 +0530

    master

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener('click', function() {
+    if (form.classList.contains("hidden")) {
+        form.classList.remove("hidden");
+    } else {
+        form.classList.add("hidden");
+    }
+});
