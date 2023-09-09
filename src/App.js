0f7a19e4539c534ac21d92c2025f2c93a23298f4
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-10 00:57:07 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-no-target-blank */
+
+import { useState } from "react";
 import "./style.css";
 
 const initialFacts = [
   {
     id: 1,
-    text: 'React is being developed by Meta (formerly facebook)',
-    source: 'https://opensource.fb.com/',
-    category: 'technology',
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
     votesInteresting: 24,
     votesMindblowing: 9,
     votesFalse: 4,
@@ -15,10 +17,10 @@ const initialFacts = [
   },
   {
     id: 2,
-    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
     source:
-      'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
-    category: 'society',
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
     votesInteresting: 11,
     votesMindblowing: 2,
     votesFalse: 0,
@@ -26,9 +28,9 @@ const initialFacts = [
   },
   {
     id: 3,
-    text: 'Lisbon is the capital of Portugal',
-    source: 'https://en.wikipedia.org/wiki/Lisbon',
-    category: 'society',
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
     votesInteresting: 8,
     votesMindblowing: 3,
     votesFalse: 1,
@@ -36,16 +38,20 @@ const initialFacts = [
   },
 ];
 
-const CATEGORIES = [
-  { name: 'technology', color: '#3b82f6' },
-  { name: 'science', color: '#16a34a' },
-  { name: 'finance', color: '#ef4444' },
-  { name: 'society', color: '#eab308' },
-  { name: 'entertainment', color: '#db2777' },
-  { name: 'health', color: '#14b8a6' },
-  { name: 'history', color: '#f97316' },
-  { name: 'news', color: '#8b5cf6' },
-];
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  return(
+    <div>
+      <span style = {{fontSize:'40px'}}>{count}</span>
+      <button className="btn btn-large" onClick = {() =>
+      setCount((count) => count + 1)}>+1</button>
+    </div>
+  );
+}
+
 
 function App() {
   const appTitle = "Today I Learned";
@@ -72,6 +78,8 @@ function App() {
         </button>
       </header>
 
+
+      <Counter />
       <NewFactForm />
 
       <main className="main">
@@ -90,8 +98,37 @@ function NewFactForm() {
   );
 }
 
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function CategoryFilter() {
-  return <aside>Category Filter</aside>;
+  return <aside>
+    <ul>
+    <li className="category">
+      <button className="btn btn-all-categories">All</button>
+      </li>
+      {CATEGORIES.map((category) =>(
+         <li key= {category.name} className="category">
+         <button
+           className="btn btn-category"
+           style={{ backgroundColor: category.color }}
+         >
+           {category.name}
+         </button>
+       </li>
+      ))}
+    
+    </ul>
+    </aside>;
 }
 
 function FactList() {
@@ -102,49 +139,40 @@ function FactList() {
     <section>
       <ul className="facts-list">
         {facts.map((fact) => (
-          <Fact key={fact.id} fact={fact}/>
+          <Fact key={fact.id} fact={fact} />
         ))}
       </ul>
+      <p>There are {facts.length} facts in the database</p>
       Facts List
     </section>
   );
 }
 
-
-
-
 function Fact({ fact }) {
-  
-  
-  
-  return(
+  return (
     <li className="fact">
-            <p>
-              {fact.text}
-              <a
-                className="source"
-                href={fact.source}
-                target="_blank"
-              >
-                (Source)
-              </a>
-            </p>
-            <span
-              className="tag"
-              style={{
-                backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color,
-              }}
-            >
-             {fact.category}
-            </span>
-            <div className="vote-buttons">
-              <button>👍 {fact.votesInteresting}</button>
-              <button>🤯 {fact.votesMindblowing}</button>
-              <button>⛔️ {fact.votesFalse}</button>
-            </div>
-          </li>
-  )
-  
-};
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)
+            .color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
+}
 
 export default App;
