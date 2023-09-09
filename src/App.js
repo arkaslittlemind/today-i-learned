5daf035f6efd0971068230939f1bbf57a5578f52
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-09 23:48:16 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import './style.css';
+import "./style.css";
 
 function App() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img
-          src="logo.png"
-          height="68"
-          width="68"
-          alt="Today I Learned Logo"
-        />
-        <h1>Today I Learned</h1>
-      </div>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+  const appTitle = "Today I Learned";
+
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large 
+        btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
 }
 
+function NewFactForm() {
+  return (
+    <form className="fact-form" action="">
+      Fact Form
+    </form>
+  );
+}
 
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
+}
 
 export default App;
